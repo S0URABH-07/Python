fa6774a9cf35a12282f2a32a03d3fb9ef5c579bf
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-30 12:18:35 +0530

    String

---
+++ Data_Types/3_problem.py
@@ -0,0 +1,16 @@
+# String
+single_quotes = 'Sourabh'
+double_quotes = "Dhakad"
+triple_quotes = """ Triple quotes are used
+ for multiple line string
+ (Useful for multi-line text)"""
+no_inside_quotes = "25"
+
+string_repetation = "Python  "
+
+print(single_quotes)
+print(double_quotes)
+print(triple_quotes)
+print(no_inside_quotes)
+print("string_concatination :",single_quotes + " " + double_quotes)
+print("String Repetition :",string_repetation * 5)
