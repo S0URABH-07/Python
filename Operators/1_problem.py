065d26e22c6c29423f7299438226c5eb825787bc
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-30 14:28:59 +0530

    Combine Operator code

---
+++ Operators/1_problem.py
@@ -0,0 +1,30 @@
+print(20 + 5)
+print(20 - 5)
+print(20 * 5)
+print(20 / 5)
+print(20 // 6)
+print(20 % 6)
+print(3 ** 4)
+
+
+print(5 == 5)
+print(5 != 10)
+print(10 >= 10)
+print(5 <= 3)
+
+age = 20
+print(age > 18 and age < 30)
+
+print(10 > 5 or 10 < 5)
+
+print(not True)
+print(not False)
+
+a = [1, 2]
+b = a
+print(a is b)
+
+a = [1, 2]
+b = [1, 2]
+print(a is not b)
+
