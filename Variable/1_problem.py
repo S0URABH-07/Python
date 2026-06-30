75ef4f561aef58373949b1f46e3dc7f67c896a8b
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-30 11:58:18 +0530

    Variable example

---
+++ Variable/1_problem.py
@@ -0,0 +1,15 @@
+employee_name = "Sourabh"
+
+employee_id = 105
+
+salary = 55000
+
+department = "Data Science"
+
+is_active = True
+
+print(employee_name)
+print(employee_id)
+print(salary)
+print(department)
+print(is_active)
