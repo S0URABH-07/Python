3311b3a7f87f8d8b4e0a73f62c737a0fadfb8e68
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-07-01 12:53:59 +0530

    Break , continue or pass

---
+++ Loops/2_problem.py
@@ -0,0 +1,16 @@
+# break , continue and pass statement
+for i in range(1, 11):
+    if i == 6:
+        break
+    print(i)
+
+
+for j in range(1, 6):
+    if j == 3:
+        continue
+    print(j)
+
+for k in range(5):
+    pass
+
+print("Program Finished")
