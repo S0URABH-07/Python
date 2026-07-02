a597d66d5de95c95b114f2f002eaae0a89832708
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-07-02 16:57:47 +0530

    Polymorphism (Method Overriding)

---
+++ OOP_Part_2/6_problem.py
@@ -0,0 +1,12 @@
+# Polymorphism (Method Overriding)
+class Animal:
+    def sound(self):
+        print("Animal Sound")
+
+
+class Dog(Animal):
+    def sound(self):
+        print("Bark")
+
+dog = Dog()
+dog.sound()
