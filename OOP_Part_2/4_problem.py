cefe43449ff1dd695d5b2df645ac49d5c6923e1e
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-07-02 16:51:41 +0530

    Multiple Inheritence

---
+++ OOP_Part_2/4_problem.py
@@ -0,0 +1,18 @@
+# Multiple Inheritence
+class Father:
+    def skills(self):
+        print("Driving")
+
+
+class Mother:
+    def talent(self):
+        print("Cooking")
+
+
+class Child(Father, Mother):
+    pass
+
+
+child = Child()
+child.skills()
+child.talent()
