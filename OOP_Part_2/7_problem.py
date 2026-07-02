ee4c729ce6b4dbd84a579853dcfdf39416446caa
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-07-02 17:01:51 +0530

    Call super fnxn

---
+++ OOP_Part_2/7_problem.py
@@ -0,0 +1,11 @@
+# Call super function 
+class Animal:
+    def __init__(self):
+        print("Animal Constructor")
+
+class Dog(Animal):
+    def __init__(self):
+        super().__init__()
+        print("Dog Constructor")
+
+dog = Dog()
