2b827d500e46f0bfa7ec71e4e2b155dc1070bec8
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-07-03 13:05:20 +0530

    Problem with CSV

--- File_Handling/4_problem.py
+++ File_Handling/4_problem.py
@@ -1,5 +1,6 @@
 # Writing a CSV file
 # This w erase all previous data than write
+# Use "a" if you want to keep existing data.
 import csv
 with open("students.csv", "w", newline="") as file:
     writer = csv.writer(file)
