d7e4a939ae87fb649853a664f24efaf1d1acbe83
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-08-08 22:55:27 +0530

    Loop

--- Loops/1_problem.py
+++ Loops/1_problem.py
@@ -1,17 +1,14 @@
 for i in range(2, 11, 2):
     print(i)
 
-# Looping Through a string
 name = "Python"
 for ch in name:
     print(ch)
 
-# Looping Through a list
 numbers = [10, 20, 30, 40]
 for n in numbers:
     print(n)
 
-# Looping Through a Dictionary
 student = {
     "name": "Sourabh",
     "age": 22,
